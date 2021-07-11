5458c61efde0e0baa1b51ec6c68a6ca7d144fd81
sedx876 <swatkins876@gmail.com>
2021-07-11 09:56:59 -0400

    Work on Backend routes and added data/characters.js file

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const characters = require('./data/characters')
 
 const app = express()
 
 app.get('/', (req, res) =>{
   res.send('API is Running')
+})
+
+app.get('/api/characters', (req, res) =>{
+  res.json(characters)
+})
+
+app.get('/api/characters/:id', (req, res) =>{
+  const character = characters.find(c => c._id === req.params.id)
+  res.json(character)
 })
 
 app.listen(5000, console.table('Server is Running on Port 5000!!'))
