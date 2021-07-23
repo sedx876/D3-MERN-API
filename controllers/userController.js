c9f7319a85d44e00b545088c0fea88f2ebded113
sedx876 <swatkins876@gmail.com>
2021-07-23 08:41:27 -0400

    Fixed jsonwebtoken issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,22 +2,22 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
-const authUser = asyncHandler(async(req, res) => {
-  const { email, password } = req.body 
-  res.send({email, password})
-  const user = await User.findOne({ email })
-  if(user && (await user.matchPassword(password))){
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      image: user.image,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    })
-  }else {
-    res.status(401)
-    throw new Error('Invalid Email and/or Password')
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+      res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id)
+      });
+  } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
   }
 })
 
@@ -60,7 +60,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      image: user.image,
       isAdmin: user.isAdmin,
     })
   } else {
